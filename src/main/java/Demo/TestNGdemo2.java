f3886df70fb8a4e2a1257092fb966df41f31ae47
Manisha <manisha.m.pai@gmail.com>
2023-12-25 10:31:42 -0600

    first commit

---
+++ src/main/java/Demo/TestNGdemo2.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class TestNGdemo2 {
+    @Test
+	public void login()
+	{
+		System.out.println("Login Successfully");
+		
+	}
+}
---
+++ src/main/java/Demo/TestNGdemo3.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class TestNGdemo3 {
+    @Test
+	public void login()
+	{
+		System.out.println("Login Successfully");
+		
+	}
+}
